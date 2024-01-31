7cac6081fe39bf8e6449ef58773c6993e83c6fca
Crxstian <cristian19overdrive@gmail.com>
2024-01-31 13:42:30 -0300

    Empleado Repository,Service y Controlador Funcionando

--- ejercicio1/src/main/java/com/example/ejercicio1/controllers/EmpleadoController.java
+++ ejercicio1/src/main/java/com/example/ejercicio1/controllers/EmpleadoController.java
@@ -1,8 +1,42 @@
 package com.example.ejercicio1.controllers;
+import com.example.ejercicio1.entity.Empleado;
+import com.example.ejercicio1.services.EmpleadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequestMapping("/empleados")
 public class EmpleadoController {
 
+    @Autowired
+    EmpleadoService empleadoService;
+
+    @GetMapping
+    public ResponseEntity<List<Empleado>> listar(){
+        List<Empleado> empleados = empleadoService.getAll();
+        return new ResponseEntity<>(empleados, HttpStatus.OK);
+    }
+
+    @PostMapping("/agregar")
+    public ResponseEntity agregarEmpleado(@RequestBody Empleado empleado){
+
+        empleadoService.save(empleado);
+        return new ResponseEntity<>("Empleado Agregado con Exito",HttpStatus.CREATED);
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity modificarEmpleado(@RequestBody Empleado empleado){
+        empleadoService.save(empleado);
+        return new ResponseEntity<>("Empleado Modificado con Exito",HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity eliminarEmpleado(@PathVariable int id){
+        empleadoService.deleteById(id);
+        return new ResponseEntity<>("Empleado Eliminado con Exito",HttpStatus.NO_CONTENT);
+    }
 }
