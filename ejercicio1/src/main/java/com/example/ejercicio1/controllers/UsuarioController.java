ae59747d51c30d81065e74a76db6116f8fe7a140
crxstian <cristian19overdrive@gmail.com>
2024-02-01 12:43:45 -0300

    Back Funcionado

--- ejercicio1/src/main/java/com/example/ejercicio1/controllers/UsuarioController.java
+++ ejercicio1/src/main/java/com/example/ejercicio1/controllers/UsuarioController.java
@@ -5,13 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
     @PostMapping("/iniciarSesion")
-    public ResponseEntity<String> iniciarSesion(@RequestParam String usuario, @RequestParam String password) {
+    public ResponseEntity<String> iniciarSesion(@RequestBody Map<String, String> datos) {
+        String usuario = datos.get("usuario");
+        String password = datos.get("password");
         boolean esValido = usuarioService.ingresar(usuario, password);
 
         if (esValido) {
