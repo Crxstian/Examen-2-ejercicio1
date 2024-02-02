87d6f4a7ab9aa67960596af6f157d827e9ae39e5
crxstian <cristian19overdrive@gmail.com>
2024-02-01 22:13:16 -0300

    Implementando Log4j

--- ejercicio1/src/main/java/com/example/ejercicio1/controllers/UsuarioController.java
+++ ejercicio1/src/main/java/com/example/ejercicio1/controllers/UsuarioController.java
@@ -1,10 +1,11 @@
 package com.example.ejercicio1.controllers;
 import com.example.ejercicio1.services.UsuarioService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @RestController
@@ -12,6 +13,7 @@ import java.util.Map;
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
+    private static final Logger LOGGER = LogManager.getLogger(UsuarioController.class);
 
     @PostMapping("/iniciarSesion")
     public ResponseEntity<String> iniciarSesion(@RequestBody Map<String, String> datos) {
@@ -20,8 +22,11 @@ public class UsuarioController {
         boolean esValido = usuarioService.ingresar(usuario, password);
 
         if (esValido) {
+            LOGGER.info("Credenciales Correctas,Login Exitoso");
             return new ResponseEntity<>("Inicio de sesión exitoso", HttpStatus.OK);
+
         } else {
+            LOGGER.info("Credenciales Incorrectas, Login Fallido");
             return new ResponseEntity<>("Inicio de sesión fallido", HttpStatus.UNAUTHORIZED);
         }
     }
--- ejercicio1/src/main/java/com/example/ejercicio1/services/EmpleadoService.java
+++ ejercicio1/src/main/java/com/example/ejercicio1/services/EmpleadoService.java
@@ -1,12 +1,17 @@
 package com.example.ejercicio1.services;
 import com.example.ejercicio1.entity.Empleado;
 import com.example.ejercicio1.repository.EmpleadoRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
+
 @Service
 public class EmpleadoService implements IService<Empleado>{
 
+    private static final Logger LOGGER = LogManager.getLogger(EmpleadoService.class);
 
     @Autowired
     EmpleadoRepository empleadoRepository;
@@ -16,11 +21,20 @@ public class EmpleadoService implements IService<Empleado>{
     }
 
     @Override
-    public void save(Empleado empleado) {empleadoRepository.save(empleado);}
+    public void save(Empleado empleado) {
+        empleadoRepository.save(empleado);
+        LOGGER.info("Empleado guardado con Exito en la Base de datos");
+    }
 
     @Override
-    public void update(Empleado empleado) {empleadoRepository.save(empleado);}
+    public void update(Empleado empleado) {
+        empleadoRepository.save(empleado);
+        LOGGER.info("Empleado Actualizado con Exito en la Base de datos");
+    }
 
     @Override
-    public void deleteById(int id) {empleadoRepository.deleteById(id);}
+    public void deleteById(int id) {
+        empleadoRepository.deleteById(id);
+        LOGGER.info("Empleado Eliminado con Exito en la Base de datos");
+    }
 }
